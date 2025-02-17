d9aacf6e5b1b5ce741892c00486677412f2e9cca
Bhavya1352 <bhavyamishra698@gmail.com>
2025-02-17 19:40:38 +0000

    datatype

---
+++ 01_basics/02_variable.js
@@ -0,0 +1,12 @@
+// "use strict"treat all js with newer version
+// alert(3+3)we are using node js not browser
+// code readiabilty should be high
+// data type
+// number ,bigint (if large no there then used),string("" always), boolean- true/false, null-(standalone value), undefined
+// null means it is not undefined it is empty 
+//  symbol -unique
+// object
+
+console.log(typeof"bhavya")
+console.log(typeof age)
+console.log(typeof null)
