a363a07fe46c51811ffa26d723236226b99a12fc
Bhavya1352 <bhavyamishra698@gmail.com>
2025-02-17 19:05:30 +0000

    upload

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+let accountID = 14453
+let accountEmail="Bhavya@goggle.com"
+let accountPassword = "44567"
+let accountCity = "jaipur"
+let accountState
+
+// accountID= 2 not allowed constant cannot be changed
+accountID = 14458
+accountEmail = "hc@hc.com"
+accountPassword = "284722"
+accountCity= "patna"
+console.log(accountID);
+
+console.table([ accountID,accountEmail ,accountPassword ,accountCity,accountState])
