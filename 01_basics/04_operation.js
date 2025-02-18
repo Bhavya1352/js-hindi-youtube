ccb7931d3d9507ab3e637482874f06f96abc15ff
Bhavya1352 <bhavyamishra698@gmail.com>
2025-02-18 08:45:04 +0000

    jkmk

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,16 @@
+let value= 3
+let negvalue = -value
+// console.log(negvalue);
+// str1=string1
+
+let str1="Bhavya" 
+let str2=" Mishra"
+
+let str3 = str1+ str2
+// console.log(str3);
+ let gameCounter =100
+//  gameCounter++
+++gameCounter
+ console.log(gameCounter);
+ 
+
