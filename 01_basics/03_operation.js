812b210faaa61dbb7e9602b6b99a60f99453c229
Bhavya1352 <bhavyamishra698@gmail.com>
2025-02-18 07:46:45 +0000

    uplo

---
+++ 01_basics/03_operation.js
@@ -0,0 +1,20 @@
+
+let score = undefined
+// console.log(typeof score)
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn ="bhavya"
+let BooleanisLoggedIn =Boolean(isLoggedIn)
+console.log(BooleanisLoggedIn);
+
+let somenumber=33
+let StringNumber = String(somenumber)
+console.log(StringNumber);
+
+
+
+
